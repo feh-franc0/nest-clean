b9fac5ef483f30f4269b85f753703af5ec10bbf6
feh-franc0 <fernandofv1110@gmail.com>
2023-09-18 17:01:19 -0300

    Banco de dados isolado nos testes

---
+++ test/setup-e2e.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const SchemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(SchemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${SchemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
